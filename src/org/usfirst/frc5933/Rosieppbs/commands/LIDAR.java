cb4a75d4d0b2bd6fba295263b9145319e8735112
Miriam317 <miriamasmith4001@gmail.com>
2016-11-20 14:12:57 -0700

    LIDAR code
    untested, should fix the problems we had before (SHOULD)

--- src/org/usfirst/frc5933/Rosieppbs/commands/LIDAR.java
+++ src/org/usfirst/frc5933/Rosieppbs/commands/LIDAR.java
@@ -11,70 +11,103 @@
 
 package org.usfirst.frc5933.Rosieppbs.commands;
 
-import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.can.CANJNI;
 import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc5933.Rosieppbs.Robot;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.Arrays;
 
 /**
  *
  */
 public class LIDAR extends Command {
-	static final int LIDARLite_ADDRESS = 0x62;
-	static final int MEASURE_REGISTER = 0x00;
-	static final int MEASURE_VALUE = 0x04;
-	static final int REGISTER_HIGHLOWB = 0x8f;
-	static final int RESPONSE_LENGTH = 2;
-
+	private enum CommandState {
+		READY, REQUEST_DISTANCE, WAIT_FOR_DISTANCE, DONE
+	}
+	
+	private CommandState state;
     public LIDAR() {
-
+    	state = CommandState.READY;
     }
 
+    /* Check the datasheets for your device for the arbitration IDs of the
+    messages you want to send.  By convention, this is a bitstring
+    containing the model number, manufacturer number, and api number. */
+	private static final int REQUEST_RANGE_ARB_ID = 0x1;
+	private static final int RESPOND_RANGE_ARB_ID = 0x2;
+	
+	/*  Device ID, from 0 to 63. */
+	private static final int LIDAR_ID = 60;
+	
+	private IntBuffer messageId = ByteBuffer.allocateDirect(4).asIntBuffer();
+	private ByteBuffer data = ByteBuffer.allocateDirect(8);
+	private ByteBuffer timestamp = ByteBuffer.allocate(4);
+	private int range = 0;
+    
     private int makeUnsignedByte(byte b) {
     	return (int) b & 0xFF;
     }
-    // Called just before this Command runs the first time
-    public void getRange() {
-    	I2C LIDARDevice;
-    	LIDARDevice = new I2C(I2C.Port.kOnboard, LIDARLite_ADDRESS);
-    	
-    	System.err.println(I2C.Port.kOnboard +"   " + LIDARLite_ADDRESS);
-    	
-    	byte[] initiateCommand = {MEASURE_REGISTER, MEASURE_VALUE};
-    	byte[] readMeasurement = new byte[RESPONSE_LENGTH];
-    	byte[] measurementReady = new byte[1];
-    	
-    	//boolean z = LIDARDevice.writeBulk(initiateCommand);
-		boolean z = LIDARDevice.write(MEASURE_REGISTER, MEASURE_VALUE);
-		try {
-			Thread.sleep(20);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		do{
-			LIDARDevice.read(0x01, 1, measurementReady);
-		} while ((readMeasurement[0] & (byte)0x1) == 0);
-			
-		//boolean y = LIDARDevice.write(REGISTER_HIGHLOWB,0x0);
-		boolean x = LIDARDevice.read(REGISTER_HIGHLOWB, RESPONSE_LENGTH, readMeasurement);
-		//boolean x = LIDARDevice.readOnly(readMeasurement, RESPONSE_LENGTH);
-		System.out.println("REturn Values " + z + " " + " " + x);
-		System.out.println("Readmeasurements " + readMeasurement[0] + ", " + readMeasurement[1]);
-		int range = (makeUnsignedByte(readMeasurement[0])<<8) + makeUnsignedByte(readMeasurement[1]);
-		System.out.println("Range is: " + range);	
+    
+    private void requestRange() {
+        /* Again, see the datasheet for the device you're using.  It should
+        specify what data to put here. */
+	    data.clear();
+	    data.putInt(1);
+	
+	    /* Alternatively, instead of CAN_SEND_PERIOD_NO_REPEAT, you can specify
+	        a period in milliseconds to automatically send the message over
+	        and over again. */
+	    CANJNI.FRCNetworkCommunicationCANSessionMuxSendMessage(
+	    		REQUEST_RANGE_ARB_ID | LIDAR_ID,
+	            data,
+	            CANJNI.CAN_SEND_PERIOD_NO_REPEAT
+	    );	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	if (state == CommandState.READY) {
+    		// Calling in ready state means command was just scheduled, so get the range
+    		requestRange();
+    		state = CommandState.WAIT_FOR_DISTANCE;
+    	}
+    	else if (state == CommandState.WAIT_FOR_DISTANCE) {
+            /* To receive a message, put the message ID you're looking for in this
+            buffer.  CANJNI...ReceiveMessage  will not block waiting for it,
+            but just return null if it hasn't been received yet. */
+	        messageId.clear();
+	        messageId.put(0, RESPOND_RANGE_ARB_ID | LIDAR_ID);
+	
+	        ByteBuffer data = CANJNI.FRCNetworkCommunicationCANSessionMuxReceiveMessage(
+	                messageId,
+	                CANJNI.CAN_MSGID_FULL_M,
+	                timestamp
+	        );
+	
+	        if (data != null) {
+	            System.out.println("Received a message: " + Arrays.toString(data.array()));
+	    		range = (makeUnsignedByte(data.array()[0])<<8) + makeUnsignedByte(data.array()[1]);
+	    		System.out.println("Range is: " + range);
+	    		state = CommandState.DONE;
+	        }
+
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	if (state == CommandState.DONE) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	state = CommandState.READY;
     }
 
     // Called when another command which requires one or more of the same
@@ -87,4 +120,9 @@ public class LIDAR extends Command {
 		// TODO Auto-generated method stub
 		
 	}
+
+	public int getRange() {
+		return range;
+	}
+
 }
